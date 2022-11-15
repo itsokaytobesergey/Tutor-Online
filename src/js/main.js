5c8c8dec91960b1669ca8d818b6c4380e8694d80
itsokaytobesergey <by.imbooo@gmail.com>
2022-11-16 00:04:06 +0500

    Final update

--- src/js/main.js
+++ src/js/main.js
@@ -72,126 +72,130 @@ menuMobileItems.forEach(e => {
   });
 });
 
-////////selector
+// ////////selector
 
-class Test {
-  constructor(selector) {
-    this.selector = selector;
+// class Test {
+//   constructor(selector) {
+//     this.selector = selector;
 
-    this.dropdownInput = selector.querySelector('[data-id="dropdownInput"]');
-    this.dropdownInputText = selector.querySelector('[data-id="dropdownInputText"]');
-    this.dropdownMenu = selector.querySelector('[data-id="subject"]');
-    this.dropdownMenuList = selector.querySelectorAll('[data-id="subject"]');
-    this.dropdownArrow = selector.querySelector('[data-id="dropdownArrow"]');
-    this.submitButton = selector.querySelector('[data-id="submitButton"]');
-    this.listItem = selector.querySelectorAll('.dropdown__item');
-    this.inputField = selector.querySelector('[data-id="input"]');
-    this.dropdownBackground = document.querySelector('[data-id="dropdownBackground"]');
-    this.dropdownTickets = selector.querySelectorAll('[data-id="dropdownTickets"]');
-    this.submitButton = document.querySelector('[data-id="submitButton"]');
-    this.errorSign = document.querySelectorAll('[data-id="error"]');
-    this.dropdownInput = document.querySelectorAll('[data-id="dropdownInput"]');
-    this.dropdownInputText = document.querySelectorAll('[data-id="dropdownInputText"]');
+//     this.dropdownInput = selector.querySelector('[data-id="dropdownInput"]');
+//     this.dropdownInputText = selector.querySelector('[data-id="dropdownInputText"]');
+//     this.dropdownMenu = selector.querySelector('[data-id="subject"]');
+//     this.dropdownMenuList = selector.querySelectorAll('[data-id="subject"]');
+//     this.dropdownArrow = selector.querySelector('[data-id="dropdownArrow"]');
+//     this.submitButton = selector.querySelector('[data-id="submitButton"]');
+//     this.listItem = selector.querySelectorAll('.dropdown__item');
+//     this.inputField = selector.querySelector('[data-id="input"]');
+//     this.dropdownBackground = document.querySelector('[data-id="dropdownBackground"]');
+//     this.dropdownTickets = selector.querySelectorAll('[data-id="dropdownTickets"]');
+//     this.submitButton = document.querySelector('[data-id="submitButton"]');
+//     this.errorSign = document.querySelectorAll('[data-id="error"]');
+//     this.dropdownInput = document.querySelectorAll('[data-id="dropdownInput"]');
+//     this.dropdownInputText = document.querySelectorAll('[data-id="dropdownInputText"]');
 
-    this.dropdownMenuNumber1 = document.querySelector('[data-number="1"]');
-    this.dropdownMenuNumber2 = document.querySelector('[data-number="2"]');
+//     this.dropdownMenuNumber1 = document.querySelector('[data-number="1"]');
+//     this.dropdownMenuNumber2 = document.querySelector('[data-number="2"]');
 
-    this.init();
-  }
+//     this.init();
+//   }
 
-  init() {
-    this.dropdownBackground.addEventListener('click', () => {
-      this.dropdownBackground.classList.add('is-hidden');
-      this.dropdownMenu.classList.add('is-hidden');
-      this.dropdownArrow.classList.remove('rotateMe');
-    });
+//   init() {
+//     this.dropdownBackground.addEventListener('click', () => {
+//       this.dropdownBackground.classList.add('is-hidden');
+//       this.dropdownMenu.classList.add('is-hidden');
+//       this.dropdownArrow.classList.remove('rotateMe');
+//     });
 
-    document.addEventListener('keydown', e => {
-      if (e.key === 'Tab' || e.key === 'Escape') {
-        this.dropdownBackground.classList.add('is-hidden');
-        this.dropdownMenu.classList.add('is-hidden');
-        this.dropdownArrow.classList.remove('rotateMe');
-      }
-    });
+//     document.addEventListener('keydown', e => {
+//       if (e.key === 'Tab' || e.key === 'Escape') {
+//         this.dropdownBackground.classList.add('is-hidden');
+//         this.dropdownMenu.classList.add('is-hidden');
+//         this.dropdownArrow.classList.remove('rotateMe');
+//       }
+//     });
 
-    this.selector.addEventListener('click', () => {
-      this.dropdownMenu.classList.toggle('is-hidden');
-      this.dropdownArrow.classList.toggle('rotateMe');
-      this.dropdownBackground.classList.toggle('is-hidden');
-    });
+//     this.selector.addEventListener('click', () => {
+//       this.dropdownMenu.classList.toggle('is-hidden');
+//       this.dropdownArrow.classList.toggle('rotateMe');
+//       this.dropdownBackground.classList.toggle('is-hidden');
+//     });
 
-    this.listItem.forEach(el => {
-      el.addEventListener('click', () => {
-        this.listItem.forEach(e => {
-          e.classList.remove('dropdown__item--choosed');
-          e.lastChild.classList.add('is-hidden');
-        });
+//     this.listItem.forEach(el => {
+//       el.addEventListener('click', () => {
+//         this.listItem.forEach(e => {
+//           e.classList.remove('dropdown__item--choosed');
+//           e.lastChild.classList.add('is-hidden');
+//         });
 
-        el.classList.add('dropdown__item--choosed');
-        el.lastChild.classList.remove('is-hidden');
+//         el.classList.add('dropdown__item--choosed');
+//         el.lastChild.classList.remove('is-hidden');
 
-        console.log(this.dropdownInputText);
-        this.dropdownInputText.innerText = el.innerText;
-        this.inputField.value = el.dataset.value;
-      });
-    });
-  }
-}
+//         console.log(this.dropdownInputText);
+//         this.dropdownInputText.innerText = el.innerText;
+//         this.inputField.value = el.dataset.value;
+//       });
+//     });
+//   }
+// }
+
+// document.querySelectorAll('[data-id="selector"]').forEach(selector => {
+//   // dropdownFunction(selector);
+//   new Test(selector);
+// });
 
 document.querySelectorAll('[data-id="selector"]').forEach(selector => {
-  // dropdownFunction(selector);
-  new Test(selector);
+  dropdownFunction(selector);
 });
 
-// function dropdownFunction(selector) {
-//   const dropdownInput = selector.querySelector('[data-id="dropdownInput"]');
-//   const dropdownInputText = selector.querySelector('[data-id="dropdownInputText"]');
-//   const dropdownMenu = selector.querySelector('[data-id="subject"]');
-//   const dropdownArrow = selector.querySelector('[data-id="dropdownArrow"]');
-//   const submitButton = selector.querySelector('[data-id="submitButton"]');
-//   const listItem = selector.querySelectorAll('.dropdown__item');
-//   const inputField = selector.querySelector('[data-id="input"]');
-//   const dropdownBackground = document.querySelector('[data-id="dropdownBackground"]');
-//   const dropdownTickets = selector.querySelectorAll('[data-id="dropdownTickets"]');
+function dropdownFunction(selector) {
+  const dropdownInput = selector.querySelector('[data-id="dropdownInput"]');
+  const dropdownInputText = selector.querySelector('[data-id="dropdownInputText"]');
+  const dropdownMenu = selector.querySelector('[data-id="subject"]');
+  const dropdownArrow = selector.querySelector('[data-id="dropdownArrow"]');
+  const submitButton = selector.querySelector('[data-id="submitButton"]');
+  const listItem = selector.querySelectorAll('.dropdown__item');
+  const inputField = selector.querySelector('[data-id="input"]');
+  const dropdownBackground = document.querySelector('[data-id="dropdownBackground"]');
+  const dropdownTickets = selector.querySelectorAll('[data-id="dropdownTickets"]');
 
-//   //тык вне поля дропдауна
-//   dropdownBackground.addEventListener('click', () => {
-//     dropdownBackground.classList.add('is-hidden');
-//     dropdownMenu.classList.add('is-hidden');
-//     dropdownArrow.classList.remove('rotateMe');
-//   });
+  //тык вне поля дропдауна
+  dropdownBackground.addEventListener('click', () => {
+    dropdownBackground.classList.add('is-hidden');
+    dropdownMenu.classList.add('is-hidden');
+    dropdownArrow.classList.remove('rotateMe');
+  });
 
-//   //закрытие дропдауна на escape & tab
-//   document.addEventListener('keydown', e => {
-//     if (e.key === 'Tab' || e.key === 'Escape') {
-//       dropdownBackground.classList.add('is-hidden');
-//       dropdownMenu.classList.add('is-hidden');
-//       dropdownArrow.classList.remove('rotateMe');
-//     }
-//   });
-//   //открытие дропдауна
-//   selector.addEventListener('click', () => {
-//     dropdownMenu.classList.toggle('is-hidden');
-//     dropdownArrow.classList.toggle('rotateMe');
-//     dropdownBackground.classList.toggle('is-hidden');
-//   });
+  //закрытие дропдауна на escape & tab
+  document.addEventListener('keydown', e => {
+    if (e.key === 'Tab' || e.key === 'Escape') {
+      dropdownBackground.classList.add('is-hidden');
+      dropdownMenu.classList.add('is-hidden');
+      dropdownArrow.classList.remove('rotateMe');
+    }
+  });
+  //открытие дропдауна
+  selector.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('is-hidden');
+    dropdownArrow.classList.toggle('rotateMe');
+    dropdownBackground.classList.toggle('is-hidden');
+  });
 
-//   //присваивание инпуту выбранного значения + подсветка выбранной строки
-//   listItem.forEach(el => {
-//     el.addEventListener('click', () => {
-//       listItem.forEach(e => {
-//         e.classList.remove('dropdown__item--choosed');
-//         e.lastChild.classList.add('is-hidden');
-//       });
+  //присваивание инпуту выбранного значения + подсветка выбранной строки
+  listItem.forEach(el => {
+    el.addEventListener('click', () => {
+      listItem.forEach(e => {
+        e.classList.remove('dropdown__item--choosed');
+        e.lastChild.classList.add('is-hidden');
+      });
 
-//       el.classList.add('dropdown__item--choosed');
-//       el.lastChild.classList.remove('is-hidden');
+      el.classList.add('dropdown__item--choosed');
+      el.lastChild.classList.remove('is-hidden');
 
-//       dropdownInputText.innerText = el.innerText;
-//       inputField.value = el.dataset.value;
-//     });
-//   });
-// }
+      dropdownInputText.innerText = el.innerText;
+      inputField.value = el.dataset.value;
+    });
+  });
+}
 
 //если ничего не выбрано, тык по кнопке даст ошибку [bubbling suka]
 const submitButton = document.querySelector('[data-id="submitButton"]');
@@ -201,20 +205,16 @@ const dropdownInputText = document.querySelectorAll('[data-id="dropdownInputText
 
 submitButton.addEventListener('click', e => {
   if (dropdownInputText[0].innerHTML === `Выберите предмет`) {
-    console.log(`error1`);
     dropdownInput[0].classList.add('error');
     errorSign[0].classList.remove('is-hidden');
   } else {
-    console.log(`nice1`);
     dropdownInput[0].classList.remove('error');
     errorSign[0].classList.add('is-hidden');
   }
   if (dropdownInputText[1].innerHTML === `Выберите роль`) {
-    console.log(`error2`);
     dropdownInput[1].classList.add('error');
     errorSign[1].classList.remove('is-hidden');
   } else {
-    console.log(`nice2`);
     dropdownInput[1].classList.remove('error');
     errorSign[1].classList.add('is-hidden');
   }
